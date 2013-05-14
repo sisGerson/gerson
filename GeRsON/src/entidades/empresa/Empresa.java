e78802d3d36d6d655ef46c697db39cab69683733
alexmesquita <alex.mesquita06@hotmail.com>
2013-05-13 21:32:57 -0300

    implementação das classes: Empresa, Funcionario e Gerente

---
+++ GeRsON/src/entidades/empresa/Empresa.java
@@ -0,0 +1,39 @@
+package entidades.empresa;
+
+import entidades.Entidade;
+
+public class Empresa extends Entidade{
+	
+	private String responsavel;
+	private String telefoneResponsavel;
+	private String descricaoServico;
+	
+	public Empresa() {
+		super();
+	}
+
+	public String getResponsavel() {
+		return responsavel;
+	}
+
+	public void setResponsavel(String responsavel) {
+		this.responsavel = responsavel;
+	}
+
+	public String getTelefoneResponsavel() {
+		return telefoneResponsavel;
+	}
+
+	public void setTelefoneResponsavel(String telefoneResponsavel) {
+		this.telefoneResponsavel = telefoneResponsavel;
+	}
+
+	public String getDescricaoServico() {
+		return descricaoServico;
+	}
+
+	public void setDescricaoServico(String descricaoServico) {
+		this.descricaoServico = descricaoServico;
+	}
+	
+}
---
+++ GeRsON/src/entidades/funcionarios/Gerente.java
@@ -0,0 +1,14 @@
+package entidades.funcionarios;
+
+public class Gerente extends Funcionario {
+
+	public Gerente() {
+		super();
+	}
+	public void cadastrarFuncionario(){
+		
+	}
+	public void cadastrarEmpresa(){
+		
+	}
+}
