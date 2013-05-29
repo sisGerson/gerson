656bb3f04e1a5d65ed517ef7854454ee384c4219
carlosTJunior <carlosjunior117@hotmail.com>
2013-05-29 20:20:42 -0300

    criação da classe de FuncionarioTeste dentro do pacote teste

---
+++ GeRsON/src/teste/FuncionarioTeste.java
@@ -0,0 +1,179 @@
+package teste;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FuncionarioTeste {
+
+	@Test
+	public void testFuncionario() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetArea() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetArea() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMatricula() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetMatricula() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDataAdmissao() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetDataAdmissao() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDataDesligamento() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetDataDesligamento() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsStatus_2() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetStatus_2() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSalario() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetSalario() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetEnderecoFuncional() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetEnderecoFuncional() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTelefoneComercial() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetTelefoneComercial() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTipoFuncionario() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetTipoFuncionario() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetNome() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetNome() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCPF_CNPJ() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetCPF_CNPJ() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetEmail() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetEmail() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSenha() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetSenha() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCEPFuncional() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetCEPFuncional() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCidadeFuncional() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetCidadeFuncional() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetUFCidadeFuncional() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetUFCidadeFuncional() {
+		fail("Not yet implemented");
+	}
+
+}
