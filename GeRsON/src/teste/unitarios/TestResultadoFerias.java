387135cbeeb5da2e22b3447a13d87a68009bd2d4
alexmesquita <alex.mesquita06@hotmail.com>
2013-07-15 16:11:51 -0300

    Teste Servlet ResultadoPedirFerias

---
+++ GeRsON/src/teste/unitarios/TestResultadoFerias.java
@@ -0,0 +1,55 @@
+package teste.unitarios;
+
+import static org.easymock.EasyMock.createMock;
+import static org.easymock.EasyMock.createNiceMock;
+import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.replay;
+import static org.easymock.EasyMock.verify;
+import static org.junit.Assert.*;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import controller.logicas.ResultadoFerias;
+
+public class TestResultadoFerias {
+
+	ResultadoFerias resultado = new ResultadoFerias();
+	
+	@Before
+	public void testResultadoFerias() {
+		assertNotNull(resultado);
+	}
+
+	@Test
+	public void testExecuta() throws Exception {
+		HttpServletRequest requestMock = requestMock();  
+        resultado.executa(requestMock, responseMock());  
+        verify(requestMock);
+	}
+	private HttpServletRequest requestMock() {  
+        HttpServletRequest requestMock = createMock(HttpServletRequest.class);  
+        expect(requestMock.getParameter("resultado")).andReturn("Sim");  
+        expect(requestMock.getParameter("idFerias")).andReturn("1");  
+        expect(requestMock.getRequestDispatcher("/index.jsp?item=24&situacao=2")).  
+            andReturn(requestDispatcher());  
+        replay(requestMock);  
+        return requestMock;  
+    }  
+  
+    private RequestDispatcher requestDispatcher() {  
+        RequestDispatcher dispatcherMock = createNiceMock(RequestDispatcher.class);  
+        replay(dispatcherMock);  
+        return dispatcherMock;  
+    }  
+  
+    private HttpServletResponse responseMock() throws Exception {  
+        HttpServletResponse responseMock = createMock(HttpServletResponse.class);  
+        replay(responseMock);  
+        return responseMock;  
+    }
+}
